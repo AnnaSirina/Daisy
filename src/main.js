5348edb76e60b358dabf3f3186ca548051db9aad
AnnaSirina <sirina.anya@gmail.com>
2019-12-16 21:28:25 +0300

    сборка webpack

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "./styles/main.pcss";
+if (process.env.NODE_ENV === "development") {
+  require("file-loader!./index.pug");
+}
+
+import "./scripts/skills";
+import "./scripts/parallax";
+import "./scripts/popup";
+import "./scripts/rewiers";
+import "./scripts/works";
